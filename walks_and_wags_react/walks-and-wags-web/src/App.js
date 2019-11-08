6c49f81cd7b891c46f78b1d7e8f861b1351f4130
lisseteventura <lisseteventura@gmail.com>
2019-11-08 17:03:47 -0500

    added bootstrap and styled-component into react

--- walks_and_wags_react/walks-and-wags-web/src/App.js
+++ walks_and_wags_react/walks-and-wags-web/src/App.js
@@ -3,22 +3,24 @@ import React from "react";
 import Home from "./Home.js";
 import Profile from "./Profile.js";
 import Settings from "./Settings.js";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <React.Fragment>
+      <Router>
         <nav>
           <Link to="/">Home</Link>
           <Link to="/profile">Profile</Link>
           <Link to="/settings">Settings</Link>
         </nav>
-        <Route exact path="/" component={Home} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/settings" component={Settings} />
-      </div>
-    </Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/settings" component={Settings} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
