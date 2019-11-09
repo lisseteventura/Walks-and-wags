6d1fe28d256f045faf21ef99698e8ba972c97648
lisseteventura <lisseteventura@gmail.com>
2019-11-09 17:04:29 -0500

    added fields to setting section

--- walks_and_wags_react/walks-and-wags-web/src/Settings.js
+++ walks_and_wags_react/walks-and-wags-web/src/Settings.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from "reactstrap";
 
 class Settings extends Component {
   render() {
     return (
-      <div>
-        <h3>Settings go here</h3>
+      <div style={{ width: "50%", margin: "10rem auto" }}>
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">First name: </InputGroupAddon>
+          <Input placeholder="first name" />
+        </InputGroup>
+        <br />
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">Last name: </InputGroupAddon>
+          <Input placeholder="last name" />
+        </InputGroup>
+        <br />
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">Mobile:</InputGroupAddon>
+          <Input placeholder="mobile number" />
+        </InputGroup>
+        <br />
+
+        <InputGroup>
+          <Input placeholder="email" />
+          <InputGroupAddon addonType="append">
+            <InputGroupText>@example.com</InputGroupText>
+          </InputGroupAddon>
+        </InputGroup>
       </div>
     );
   }
